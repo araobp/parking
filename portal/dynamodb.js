64714de7f352d1fbe93c4fc4818ca5ca00f350aa
Shinya Arao <araobp@users.noreply.github.com>
2016-06-13 23:06:59 +0900

    bug fix

--- portal/dynamodb.js
+++ portal/dynamodb.js
@@ -13,6 +13,9 @@ exports.save = function(license_number, park_time, floor_id) {
 };
 
 exports.find = function(CarId, fn) {
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  var yesterday = date.getTime();
   var params = {
     TableName: 'ALPR',
     Limit: 5,
@@ -20,6 +23,10 @@ exports.find = function(CarId, fn) {
       CarId : {
         ComparisonOperator: 'EQ',
         AttributeValueList: [{S: CarId}]
+      },
+      Timestamp: {
+        ComparisonOperator: 'GT',
+        AttributeValueList: [{S: String(yesterday)}]
       }
     }
   };
@@ -29,20 +36,17 @@ exports.find = function(CarId, fn) {
     var items = result.Items;
     var record = {};
     var confidenceMax = 0;
-    var date = new Date();
-    date.setDate(date.getDate() - 1);
-    var yesterday = date.getTime();
-    for (var i = 0; i < items.length; i++ ) {
-      var item = items[i];
+    items.forEach(function(item) {
       var confidence = Number(item.payload.M.confidence.N);
       var timestamp = Number(item.Timestamp.S);
-      if (timestamp > yesterday && confidence > confidenceMax) {
+      console.log(timestamp, yesterday, confidence, confidenceMax);
+      if (confidence > confidenceMax) {
         confidenceMax = confidence;
         record.confidence = confidence.toFixed(1);
         record.timestamp = timestamp;
         record.site_id = item.payload.M.site_id.S;
       }
-    }
+    });
     console.log(JSON.stringify(record));
     console.log('The car(' + CarId + ') is on ' + record.site_id);
     fn(record);
